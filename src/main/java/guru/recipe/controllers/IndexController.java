9483b0539084869f033554424e0ff83564ec5807
NikolayNN <kolya.by@mail.ru>
2017-10-07 17:58:59 +0300

    use bootstrap framework for index page

--- src/main/java/guru/recipe/controllers/IndexController.java
+++ src/main/java/guru/recipe/controllers/IndexController.java
@@ -1,16 +1,9 @@
 package guru.recipe.controllers;
 
-import guru.recipe.domain.Category;
-import guru.recipe.domain.UnitOfMeasure;
-import guru.recipe.repositories.CategoryRepository;
-import guru.recipe.repositories.RecipeRepository;
-import guru.recipe.repositories.UnitOfMeasureRepository;
 import guru.recipe.servicies.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * @author Nikolay Horushko
@@ -26,7 +19,7 @@ public class IndexController {
 
     @RequestMapping({"","/","index"})
     public String getIndexPage(Model model){
-        model.addAttribute("recipies", recipeService.getRecipes());
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
