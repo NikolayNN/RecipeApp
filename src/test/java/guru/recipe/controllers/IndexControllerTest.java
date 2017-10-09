f465e9e2261ac36db2cc3b19c2377b73af003c6a
NikolayNN <kolya.by@mail.ru>
2017-10-09 22:16:58 +0300

    tests for controller and service

---
+++ src/test/java/guru/recipe/controllers/IndexControllerTest.java
@@ -0,0 +1,62 @@
+package guru.recipe.controllers;
+
+import guru.recipe.domain.Recipe;
+import guru.recipe.servicies.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Nikolay Horushko
+ */
+public class IndexControllerTest {
+
+    private IndexController indexController;
+    @Mock
+    private RecipeService recipeService;
+    @Mock
+    private Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+//        given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+//        then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+    }
+
+}
---
+++ src/test/java/guru/recipe/domain/CategoryTest.java
@@ -0,0 +1,39 @@
+package guru.recipe.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Nikolay Horushko
+ */
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setup(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId(){
+        Long idValue =4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription(){
+
+    }
+
+    @Test
+    public void getRecipes(){
+
+    }
+
+}
