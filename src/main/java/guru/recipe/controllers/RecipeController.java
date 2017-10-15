4334de60d4ed15aad6f4e7538ea542c433e95236
NikolayNN <kolya.by@mail.ru>
2017-10-15 22:51:41 +0300

    implemented adding recipe to database from web form

--- src/main/java/guru/recipe/controllers/RecipeController.java
+++ src/main/java/guru/recipe/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package guru.recipe.controllers;
 
+import guru.recipe.command.RecipeCommand;
 import guru.recipe.domain.Recipe;
 import guru.recipe.servicies.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -19,7 +22,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show/")
     public String getRecipeById(@PathVariable Long id, Model model){
 
         Recipe recipe = recipeService.getRecipeById(id);
@@ -27,5 +30,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipe);
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
