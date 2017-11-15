306d24155dfb64ac897e84c2758f46b4bc14572c
NikolayNN <kolya.by@mail.ru>
2017-11-15 20:12:21 +0300

    added validation for RecipeCommand

--- src/test/java/guru/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/guru/recipe/controllers/RecipeControllerTest.java
@@ -8,11 +8,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,20 +30,21 @@ public class RecipeControllerTest {
     private RecipeController recipeController;
     @Mock
     private RecipeService recipeService;
-
+    MockMvc mockMvc;
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
         recipeController = new RecipeController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
     public void getRecipeById() throws Exception {
         RecipeCommand recipeCommand = new RecipeCommand();
         recipeCommand.setId(1L);
-
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 
         when(recipeService.findRecipeCommandById(anyLong())).thenReturn(recipeCommand);
 
@@ -50,11 +55,25 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void saveRecipe() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeService.saveRecipeCommand(any(RecipeCommand.class))).thenReturn(recipeCommand);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some description")
+                .param("directions", "some directions"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/1/show/"));
+    }
+
+    @Test
     public void get404ErrorIfRecipeNotFound() throws Exception {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
-
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 
         when(recipeService.findRecipeById(anyLong())).thenThrow(NotFoundException.class);
 
@@ -65,8 +84,6 @@ public class RecipeControllerTest {
     @Test
     public void get404ErrorPage() throws Exception {
 
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
-
         when(recipeService.findRecipeCommandById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/recipe/888/show/"))
@@ -76,10 +93,6 @@ public class RecipeControllerTest {
 
     @Test
     public void get400ErrorPage() throws Exception {
-
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController)
-                .setControllerAdvice(new ControllerExceptionHandler())
-                .build();
 
         when(recipeService.findRecipeCommandById(anyLong())).thenThrow(NumberFormatException.class);
 
